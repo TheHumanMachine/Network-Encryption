7197199d3318c1deca9ddee564364bc4bb9428bc
francisco robles-fonseca <ffrancisc92@gmail.com>
2017-06-30 15:09:40 -0700

    Blank Build

---
+++ Networking Encryption/Networking Encryption/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Build: 0.0.0
+ * Date: 6/30/17
+ * Code Metrics:
+ * Network Encryption: 100  2   1   0   1
+ */
+
+namespace Networking_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
