a1ca19aab5be5024704ac9301ec6743aa2e15125
francisco robles-fonseca <ffrancisc92@gmail.com>
2017-06-30 23:32:03 -0700

    Build 0.1.0 addition of extention function

--- Networking Encryption/Networking Encryption/Encryption.cs
+++ Networking Encryption/Networking Encryption/Encryption.cs
@@ -3,11 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Security.Cryptography;
+using System.IO;
+/*
+ * Build: 0.1.0
+ * Date: 6/30/17
+ * Code Metrics:
+ * Network Encryption: 90  16   1   3   24
+ */
 namespace Networking_Encryption
 {
     public class Encryption
     {
+        /// <summary>
+        /// function compares the file extentions of two files
+        /// <para/>returns true if they hold the same extention 
+        /// </summary>
+        /// <param name="fileOne"> file name one </param>
+        /// <param name="fileTwo">file name two</param>
+        /// <returns> returns true if the two files contain the same extention </returns>
+        private bool checkExtention(string fileOne,string fileTwo)
+        {
+            bool areSame = false;
+            if (fileOne.Count() >= 4 && fileTwo.Count() >= 4) // 4 is the minimal len for a valid extention
+            {
+                if (getExtention(fileOne) == getExtention(fileTwo))
+                {
+                    areSame = true;
+                }
+            }
+            return areSame;
+        }
+        /// <summary>
+        /// function finds the extention type of a given file name
+        /// <para/>returns a string with the extention type of the file
+        /// </summary>
+        /// <param name="file"> name of the file</param>
+        /// <returns> returns a string associated with the file extention type</returns>
+        private string getExtention(string file)
+        {
+            int index = file.Count() - 1;
+            string extention = "";
+            char token = ' ';
+            while (token != '.' && index > 0)
+            {
+                token = file[index];
+                extention = extention.Insert(0,token.ToString());
+                index--;
+            }
+            if (!extention.Contains('.'))
+            {
+                throw new FormatException("not a valid extention");
+            }
+            return extention;
+        }
         public string Encrypt(string temp,string seed = "")
         {
             throw new NotImplementedException();
--- Networking Encryption/Networking Encryption/Program.cs
+++ Networking Encryption/Networking Encryption/Program.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
- * Build: 0.0.0
+ * Build: 0.1.0
  * Date: 6/30/17
  * Code Metrics:
- * Network Encryption: 98  8   1   1   7
+ * Network Encryption: 90  16   1   3   24
  * Unit Tests: 61   28   1   5   268
  */
 
