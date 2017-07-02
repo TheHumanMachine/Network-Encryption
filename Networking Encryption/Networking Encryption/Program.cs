9a15a40b24aa1f27dfd034984fca08b58022a47c
francisco robles-fonseca <ffrancisc92@gmail.com>
2017-07-02 14:01:12 -0700

    Build 0.3.0 append  key & iv to  str

--- Networking Encryption/Networking Encryption/Program.cs
+++ Networking Encryption/Networking Encryption/Program.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
- * Build: 0.2.0
- * Date: 6/30/17
+ * Build: 0.3.0
+ * Date: 7/2/17
  * Code Metrics:
  * Network Encryption:  76  47  1   15  110
  * Unit Tests: 61   28   1   5   268
@@ -17,9 +17,13 @@ namespace Networking_Encryption
     {
         static void Main(string[] args)
         {
+            //random test code
             Encryption e = new Encryption();
             while (true)
             {
+                byte[] c = { 1,5};
+                e.Key = c;
+                e.IV = c;
                 var a = e.Encrypt("this is a test");
                 var b = a.Length;
             }
