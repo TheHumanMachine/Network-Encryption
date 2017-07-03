1f76dd0bc45814e3ea904af575b845ed453cc655
francisco robles-fonseca <ffrancisc92@gmail.com>
2017-07-02 22:57:47 -0700

    Build 0.4.2 Blank Main

--- Networking Encryption/Networking Encryption/Program.cs
+++ Networking Encryption/Networking Encryption/Program.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
- * Build: 0.3.0
+ * Build: 0.4.2
  * Date: 7/2/17
  * Code Metrics:
- * Network Encryption:  76  47  1   15  110
+ * Network Encryption:  78  56  1   14  136
  * Unit Tests: 61   28   1   5   268
  */
 
@@ -17,16 +17,6 @@ namespace Networking_Encryption
     {
         static void Main(string[] args)
         {
-            //random test code
-            Encryption e = new Encryption();
-            while (true)
-            {
-                byte[] c = { 1,5};
-                e.Key = c;
-                e.IV = c;
-                var a = e.Encrypt("this is a test");
-                var b = a.Length;
-            }
         }
     }
 }
