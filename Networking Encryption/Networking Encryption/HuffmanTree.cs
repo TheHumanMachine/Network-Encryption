8f65c8bda4c82c04eb60b1cca88c0cb315a89b23
Sarakvitou Sam <sarakvitou@gmail.com>
2017-08-28 13:34:27 -0700

    hh

--- Networking Encryption/Networking Encryption/HuffmanTree.cs
+++ Networking Encryption/Networking Encryption/HuffmanTree.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Networking_Encryption
 {
-    class HuffmanTree
+    public class HuffmanTree
     {
+        public void encode()
+        { 
+            throw new NotImplementedException();
+        }
+        public void decode()
+        {
+            throw new NotImplementedException();
+        }
+        private int calcFreqnt()
+        {
+            throw new NotImplementedException();
+        }
+        public void saveToString()
+        {
+            throw new NotImplementedException();
+        }
+        public int size()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
