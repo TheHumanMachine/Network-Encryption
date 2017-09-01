bafe5fbad4bf4da5aa45596dc508deb7bb85911e
francisco robles-fonseca <ffrancisc92@gmail.com>
2017-09-01 12:01:14 -0700

    DES encryptor

--- Networking Encryption/Networking Encryption/DesEncryption.cs
+++ Networking Encryption/Networking Encryption/DesEncryption.cs
@@ -11,23 +11,54 @@ namespace Networking_Encryption
 {
     class DesEncryption
     {
-        private byte[] Key = null;
-        private byte[] Seed = null;
+        private byte[] key = null;
+        private byte[] seed = null;
         private SymmetricAlgorithm symetricAlgo;
-
+        /// <summary>
+        /// sets the algorithm for the DES encryptor
+        /// </summary>
         public SymmetricAlgorithm SymetricAlgo
         {
             set { symetricAlgo = value; }
         }
-        public void TextFileEncrypt(string data, string saveLocation, string seed)
+        /// <summary>
+        /// DES key property
+        /// </summary>
+        public byte[] Key
+        {
+            set { key = value; }
+            get { return key; }
+        }
+        /// <summary>
+        /// DES seed property
+        /// </summary>
+        public byte[] Seed
+        {
+            set { seed = value; }
+            get { return seed; }
+        }
+        /// <summary>
+        /// generates a key and Seed from the given seed if the seed != "" else generates a random key and Seed
+        /// </summary>
+        /// <param name="seed"></param>
+        private void genKeys(string seed = "")
+        {
+            genSeed();
+            GenKey();
+        }
+        /// <summary>
+        /// encrypts given data to a save location
+        /// </summary>
+        /// <param name="data">information to encrypt</param>
+        /// <param name="saveLocation">path to save the data</param>
+        /// <param name="seed">place to start the algo from</param>
+        public void TextFileEncrypt(string data, string saveLocation, string seed = "")
         {
             byte[] encodedData = Encoding.Unicode.GetBytes(data);
             using (FileStream fileStrm = new FileStream(saveLocation, FileMode.Create, FileAccess.Write))
             {
-                GenerateSecretInitVector();
-                GenKey();
-                //genSeed;
-                using (ICryptoTransform transform = symetricAlgo.CreateEncryptor(Key, Seed))
+                genKeys(seed);
+                using (ICryptoTransform transform = symetricAlgo.CreateEncryptor(key, this.seed))
                 {
                     using (CryptoStream cryptoStrm = new CryptoStream(fileStrm, transform, CryptoStreamMode.Write))
                     {
@@ -36,6 +67,9 @@ namespace Networking_Encryption
                 }
             }
         }
+        /// <summary>
+        /// generates a new for DES
+        /// </summary>
         private void GenKey()
         {
             if (null != (symetricAlgo as TripleDESCryptoServiceProvider))
@@ -44,7 +78,7 @@ namespace Networking_Encryption
                 tdes = symetricAlgo as TripleDESCryptoServiceProvider;
                 tdes.KeySize = 192; // Maximum key size
                 tdes.GenerateKey();
-                Key = tdes.Key;
+                key = tdes.Key;
             }
             else if (null != (symetricAlgo as RijndaelManaged))
             {
@@ -52,56 +86,50 @@ namespace Networking_Encryption
                 rdProvider = symetricAlgo as RijndaelManaged;
                 rdProvider.KeySize = 256; // Maximum key size
                 rdProvider.GenerateKey();
-                Key = rdProvider.Key;
+                key = rdProvider.Key;
             }
         }
-        private void GenerateSecretInitVector()
+        /// <summary>
+        /// generates a new seed for DES
+        /// </summary>
+        private void genSeed()
         {
             if (null != (symetricAlgo as TripleDESCryptoServiceProvider))
             {
                 TripleDESCryptoServiceProvider tdes;
                 tdes = symetricAlgo as TripleDESCryptoServiceProvider;
                 tdes.GenerateIV();
-                Seed = tdes.IV;
+                seed = tdes.IV;
             }
             else if (null != (symetricAlgo as RijndaelManaged))
             {
                 RijndaelManaged rdProvider;
                 rdProvider = symetricAlgo as RijndaelManaged;
                 rdProvider.GenerateIV();
-                Seed = rdProvider.IV;
+                seed = rdProvider.IV;
             }
         }
+        /// <summary>
+        /// functions decrypts a given path
+        /// <para> returns a decrypted string</para>
+        /// </summary>
+        /// <param name="path">name of the file</param>
+        /// <returns>returns a decrypted string</returns>
         public string decryptFile(String path)
         {
             string decrypted = "";
             // Create file stream to read encrypted file back.
             using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
             {
-                // Print out the contents of the encrypted file.
-                using (BinaryReader binReader = new BinaryReader(fileStream))
-                {
-                    int count = Convert.ToInt32(binReader.BaseStream.Length);
-
-                    byte[] bytes = binReader.ReadBytes(count);
-                    char[] array = Encoding.Unicode.GetChars(bytes);
-                    string encdata = new string(array);
-                }
-                // Reset the file stream.
                 fileStream.Seek(0, SeekOrigin.Begin);
-                // Create decryptor.
-                using (ICryptoTransform transform = symetricAlgo.CreateDecryptor(Key,Seed))
+                using (ICryptoTransform transform = symetricAlgo.CreateDecryptor(key,seed))
                 {
-                    using (CryptoStream cryptoStream = new CryptoStream(fileStream,
-                    transform,
-                    CryptoStreamMode.Read))
+                    using (CryptoStream cryptoStream = new CryptoStream(fileStream, transform, CryptoStreamMode.Read))
                     {
-                        // Print out the contents of the decrypted file.
-                        StreamReader srDecrypted = new StreamReader(cryptoStream,
-                        new UnicodeEncoding());
-                        Console.WriteLine("---------- Decrypted Data ---------");
-                        decrypted = srDecrypted.ReadToEnd();
-                        Console.WriteLine("---------- Decrypted Data ---------");
+                        using (StreamReader strmDec = new StreamReader(cryptoStream, new UnicodeEncoding()))
+                        {
+                            decrypted = strmDec.ReadToEnd();
+                        }
                     }
                 }
             }
